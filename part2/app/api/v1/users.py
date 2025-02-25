ae31281628591fccf8b34cae45139d28ec051c65
Pmichel74 <patrick.yann.michel@gmail.com>
2025-02-25 11:58:28 +0100

    merge all coworkers files

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -1,34 +1,7 @@
-#Debut partie Rayane
-#!/usr/bin/python3
-"""User API routes"""
-
-from flask import Flask, jsonify, abort
-from models import storage
-from models.user import User
-
-app = Flask(__name__)
-
-@app.route('/api/v1/users/', methods=['GET'])
-def get_users():
-    """Retrieve all users"""
-    users = storage.all(User).values()
-    return jsonify([user.to_dict() for user in users]), 200
-
-@app.route('/api/v1/users/<user_id>', methods=['GET'])
-def get_user(user_id):
-    """Retrieve a user by ID"""
-    user = storage.get(User, user_id)
-    if not user:
-        abort(404, description="User not found")
-    return jsonify(user.to_dict()), 200
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
-#Fin partie Rayane
-
 from flask_restx import Namespace, Resource, fields
 from app.services.facade import facade
+from models import storage
+from models.user import User
 import re
 
 api = Namespace('users', description='User operations')
@@ -44,6 +17,16 @@ user_model = api.model('User', {
 
 @api.route('/')
 class UserList(Resource):
+    @api.marshal_list_with(user_output_model)
+    @api.doc(responses={
+        200: 'Success',
+        404: 'No users found'
+    })
+    def get(self):
+        """Retrieve all users"""
+        users = storage.all(User).values()
+        return [user.to_dict() for user in users], 200
+
     @api.expect(user_input_model, validate=True)
     @api.marshal_with(user_output_model, code=201)
     @api.doc(responses={
@@ -60,8 +43,22 @@ class UserList(Resource):
             return new_user.to_dict(), 201
         except Exception as e:
             api.abort(400, str(e))
+
+
 @api.route('/<string:user_id>')
 class UserResource(Resource):
+    @api.marshal_with(user_output_model)
+    @api.doc(responses={
+        200: 'Success',
+        404: 'User not found'
+    })
+    def get(self, user_id):
+        """Retrieve a user by ID"""
+        user = storage.get(User, user_id)
+        if not user:
+            api.abort(404, "User not found")
+        return user.to_dict(), 200
+
     @api.expect(update_user_model)
     @api.response(200, 'User successfully updated')
     @api.response(404, 'User not found')
