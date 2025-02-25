5cbdedaee8d24595931af820608cae96a3b75c9b
RayaneAll <9975@holbertonstudents.com>
2025-02-25 14:49:23 +0100

    Add new features to the app : - Initializing Flask-RESTx with the “amenities” namespace. - Setting the amenity_model for Swagger. - Implementation of GET /api/v1/amenities/ :     - Retrieves all amenities from storage.     - Returns the list in JSON format with 200 OK.

--- part2/app/api/v1/amenities.py
+++ part2/app/api/v1/amenities.py
@@ -1,5 +1,5 @@
-from flask import jsonify
-from flask_restx import Namespace, Resource
+from flask import jsonify, request
+from flask_restx import Namespace, Resource, fields
 from models import storage
 from models.amenity import Amenity
 
@@ -11,3 +11,15 @@ amenity_model = api.model("Amenity", {
     "id": fields.String(required=True, description="Amenity ID"),
     "name": fields.String(required=True, description="Amenity name"),
 })
+
+
+@api.route("/")
+class AmenityList(Resource):
+    """Handles operations for the list of amenities"""
+
+    @api.doc("get_amenities")
+    @api.marshal_list_with(amenity_model)
+    def get(self):
+        """Retrieve all amenities"""
+        amenities = storage.all(Amenity).values()
+        return [amenity.to_dict() for amenity in amenities], 200
